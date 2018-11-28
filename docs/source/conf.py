b4cdb0caeec1a6b89cc0c65fb97be8a2aaf9f577
Christiaan Boersma <Christiaan.Boersma@nasa.gov>
2018-11-28 14:05:12 -0800

    Fixing Travis build by adding dummy test and applying flake8 correction.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -164,7 +164,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'amespahdbpythonsuite.tex', u'AmesPAHdbPythonSuite Documentation',
+    (master_doc,
+     'amespahdbpythonsuite.tex', u'AmesPAHdbPythonSuite Documentation',
      u'Christiaan Boersma and Matthew J. Shannon', 'manual'),
 ]
 
