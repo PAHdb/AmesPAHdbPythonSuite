2ab875c472d6e1e0752f0e97f979c3c5862d3921
PAHdb <Christiaan.Boersma@nasa.gov>
2021-06-01 10:49:02 -0700

    Update syntax to add custom stylesheet to Sphinx.

--- docs/conf.py
+++ docs/conf.py
@@ -112,8 +112,12 @@ html_show_sourcelink = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-def setup(app):
-    app.add_stylesheet('css/custom.css')
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
