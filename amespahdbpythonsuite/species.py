cb1e2fdbfef8ba48ce793aef0751d447bef50dec
alexmaragko <maragkoudakis.alex@gmail.com>
2021-10-01 15:18:51 +0300

    Added python3 path

--- amespahdbpythonsuite/species.py
+++ amespahdbpythonsuite/species.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 class Species():
     """
     AmesPAHdbPythonSuite species class
--- amespahdbpythonsuite/transitions.py
+++ amespahdbpythonsuite/transitions.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 class Transitions():
     """
     AmesPAHdbPythonSuite transitions class
