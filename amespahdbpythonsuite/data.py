a18575af933b14b57955ac9d549d2cb1f3d63201
alexmaragko <maragkoudakis.alex@gmail.com>
2021-11-08 15:07:21 +0200

    data.py and spectrum.py mock modules added.

---
+++ amespahdbpythonsuite/data.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+class Data():
+    """
+    AmesPAHdbPythonSuite data class
+
+    """
+
+    def __init__(self, d=None, **keywords):
+        return None
---
+++ amespahdbpythonsuite/spectrum.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+class Spectrum():
+    """
+    AmesPAHdbPythonSuite spectrum class
+
+    """
+
+    def __init__(self, d=None, **keywords):
+        return None
